b001344650a0c063f1e23ef3e381354831cf08d2
Miguel_Pelaez <Miguelpela222@gmail.com>
2023-02-03 18:12:48 -0500

    CarpetaMiguel
    Carpetas Migue

---
+++ Assets/Codigo/Mov1_1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov1_1 : MonoBehaviour
+{
+    public int limiteX;
+    public int limiteY;
+
+    public GameObject PrefabMapa;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
