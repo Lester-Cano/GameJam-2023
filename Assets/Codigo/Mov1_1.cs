29facf7fdb4c8a14f01b9e2feea2fd051033ec57
Miguel_Pelaez <Miguelpela222@gmail.com>
2023-02-03 20:25:40 -0500

    PreubaMapa#1

--- Assets/Codigo/Mov1_1.cs
+++ Assets/Codigo/Mov1_1.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class Mov1_1 : MonoBehaviour
 {
-    public int limiteX;
-    public int limiteY;
-
-    public GameObject PrefabMapa;
-
+    public int TamañoPlataforma = 3;
     
+    public Vector3 limiteCrea;
+ 
+    
+    
+    public GameObject PrefabMapa;
+  
+
     // Start is called before the first frame update
     void Start()
     {
-      
+  
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (limiteCrea.x != TamañoPlataforma && limiteCrea.y != TamañoPlataforma)
+        {
+            for (int i = 0; i < TamañoPlataforma; i++)
+            {
+                limiteCrea = new Vector3(i, 0, 0);
+                if (i >= TamañoPlataforma)
+                {
+                    for (int a = 0; a < TamañoPlataforma; a++)
+                    {
+                        limiteCrea = new Vector3(0, 0, a);
+                        Instantiate<GameObject>(PrefabMapa, limiteCrea, PrefabMapa.transform.rotation);
+                    }
+                }
+                else
+                {
+                    Instantiate<GameObject>(PrefabMapa, limiteCrea, PrefabMapa.transform.rotation);
+                }
+
+
+            }
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+
+        }
     }
 }
