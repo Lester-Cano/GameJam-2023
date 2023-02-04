1b1e5b049898cdcde1c118652ad4aed5f7ad5d85
Mateo Jimenez Fama <mateojimenezfamaart@gmail.com>
2023-02-03 23:27:22 -0500

    Esquema UI Completado
    Teo :D

--- Assets/Teo/Scripts/Disabler.cs
+++ Assets/Teo/Scripts/Disabler.cs
@@ -7,12 +7,7 @@ public class Disabler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive (false);
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
