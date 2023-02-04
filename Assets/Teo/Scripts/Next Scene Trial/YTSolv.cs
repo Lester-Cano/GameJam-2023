cc2e3486ab74060a81cce0147664d79eb834aa5e
Mateo Jimenez Fama <mateojimenezfamaart@gmail.com>
2023-02-04 15:07:07 -0500

    UI Prefab and Start of Animation2
    Teo

--- Assets/Teo/Scripts/Next Scene Trial/YTSolv.cs
+++ Assets/Teo/Scripts/Next Scene Trial/YTSolv.cs
@@ -7,20 +7,25 @@ public class YTSolv : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
-
+    public int levelIndex = 0;
 
     public void LoadNextLevel()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadLevel());
+        
     }
 
-    IEnumerator LoadLevel (int levelIndex)
+    IEnumerator LoadLevel ()
     {
-        transition.SetTrigger("Start");
-
         yield return new WaitForSeconds(transitionTime);
 
-        SceneManager.LoadScene(levelIndex);
+        transition.SetTrigger("Start");
+
+
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex+1);
+
+
 
     }
 }
