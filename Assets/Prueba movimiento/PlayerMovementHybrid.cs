0b0f312283779668c00c3e15cb8f49ad945c7e85
Lester C <nestcano2971@gmail.com>
2023-02-03 19:11:57 -0500

    PlayerMovement

--- Assets/Prueba movimiento/PlayerMovementHybrid.cs
+++ Assets/Prueba movimiento/PlayerMovementHybrid.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlayerMovementHybrid : MonoBehaviour
 {
-    
-    void Start()
+
+    PlayerInput playerInput;
+
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    bool isMovementPressed;
+
+    void Awake()
     {
-        
+        playerInput = new PlayerInput();
+
+        playerInput.CharacterControls.Movement.started += context => 
+        {
+
+            Debug.Log(context.ReadValue<Vector2>()); 
+
+        };
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
     }
 
     // Update is called once per frame
