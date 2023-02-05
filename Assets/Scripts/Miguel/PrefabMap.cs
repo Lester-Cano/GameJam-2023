88f3d7c11371a22ac2b2c55d3a449983946c2db9
Miguel_Pelaez <Miguelpela222@gmail.com>
2023-02-05 06:17:36 -0500

    Organizacion scriptsa lista

--- Assets/Scripts/Miguel/PrefabMap.cs
+++ Assets/Scripts/Miguel/PrefabMap.cs
@@ -6,22 +6,82 @@ public class PrefabMap : MonoBehaviour
 {
     public Material MatBase;
 
-    
-   
+    public Mov1_1 main;
+
+
+    [Header("Apartado de Daño")]
+
+    public GameObject MaloPrefOn;
+    public GameObject MalooPrefOff;
+
+    [Header("Bueno")]
+
+    public GameObject BuenoPrefOn;
+    public GameObject BuenoPrefOff;
+
+    [Header("Daños")]
+
+    public GameObject Puas;
+    public float TiempoEspamPuas;
+
+
+    public float TimeAnimEspera;
+
+
+    public float tiempoSpamLvl1;
+    public float tiempoSpamLvl2;
+    public float tiempoSpamLvl3;
+
+
 
 
     // Start is called before the first frame update
-
-    void Start()
+    private void Awake()
     {
         
+        main = FindObjectOfType<Mov1_1>();
+
+    }
+    void Start()
+    {
+
+        MaloPrefOn.SetActive(false);
+        MalooPrefOff.SetActive(false);
+
+        BuenoPrefOn.SetActive(false);
+        BuenoPrefOff.SetActive(false);
+
+        Puas.SetActive(false);   
+              
     }
     private void Update()
     {
         
     }
     // Update is called once per frame
+    IEnumerator RandomLvl1()
+    {
+        yield return new WaitForSeconds(tiempoSpamLvl1);
+        
+       
+            Puas.SetActive(true);
 
-  
+       
+        yield return new WaitForSeconds(tiempoSpamLvl1);
+        if (main.evento == false)
+        {
+            Puas.SetActive(false);
+            StartCoroutine(RandomLvl1());
+        }
+        else if (main.evento == true )
+        {
 
+        }
+
+    }
+
+    public void Encender() 
+    {
+        StartCoroutine(RandomLvl1());
+    }
 }
